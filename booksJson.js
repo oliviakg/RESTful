947aeba34c81347c2106320e4a4cd5970ffc7d51
Olivia Gosselin <oliviakg@umich.edu>
2019-06-26 10:58:03 -0400

    GET data

---
+++ booksJson.js
@@ -0,0 +1,20 @@
+db.books.insert([
+  {
+    title: 'Educated: A Memoir',
+    genre: 'Biography',
+    author: 'Tara Westover',
+    read: false,
+  },
+  {
+    title: 'Mrs. Everything',
+    genre: 'Fiction',
+    author: 'Jennifer Weiner',
+    read: false,
+  },
+  {
+    title: 'City of Girls',
+    genre: 'Fiction',
+    author: 'Elizabeth Gilber',
+    read: false,
+  },
+]);
